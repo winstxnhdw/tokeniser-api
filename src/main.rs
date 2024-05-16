8f15cdf26ad75fd55e2b6d955875139be977c81c
winstxnhdw <winstxnhdw@gmail.com>
2024-05-17 01:20:33 +0800

    chore: initialise repository

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use axum::{serve, Router};
+use utoipa::OpenApi;
+
+#[derive(OpenApi)]
+#[openapi(info(description = "An API"))]
+struct ApiSpecification;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let app = Router::new().merge(
+        utoipa_swagger_ui::SwaggerUi::new("/docs")
+            .url("/api-docs/openapi.json", ApiSpecification::openapi()),
+    );
+
+    let port = std::env::var("SERVER_PORT").unwrap_or_else(|_| String::from("5555"));
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{port}")).await?;
+    serve(listener, app.into_make_service()).await
+}
