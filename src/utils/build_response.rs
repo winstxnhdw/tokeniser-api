3dc364102050c3f632d076ff689643e617976d4a
winstxnhdw <winstxnhdw@gmail.com>
2024-06-26 07:12:09 +0800

    refactor: use status code struct

--- src/utils/build_response.rs
+++ src/utils/build_response.rs
@@ -1,4 +1,5 @@
 use axum::http::header::CONTENT_TYPE;
+use axum::http::StatusCode;
 use axum::response::{IntoResponse, Response};
 use serde::Serialize;
 use serde_json::to_string;
@@ -6,12 +7,12 @@ use serde_json::to_string;
 pub fn build_response<T: Serialize>(body: Option<T>, error_message: &str) -> impl IntoResponse {
     match body {
         Some(body) => Response::builder()
-            .status(200)
+            .status(StatusCode::OK)
             .header(CONTENT_TYPE, "application/json")
             .body(to_string(&body).unwrap())
             .unwrap(),
         _ => Response::builder()
-            .status(400)
+            .status(StatusCode::BAD_REQUEST)
             .header(CONTENT_TYPE, "application/json")
             .body(
                 to_string(&crate::schemas::ErrorResponseSchema {
